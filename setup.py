141e1bc90463d79ab08d6fef294b0159126a52e3
somen030 <somen030@gmail.com>
2026-01-11 11:36:26 +0530

    First set up

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace('\n',"") for req in requirements]
+
+    return requirements
+
+
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    aythor='krish',
+    author_email='somen030@gmail.com',
+    pakages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
